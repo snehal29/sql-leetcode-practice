a6e5c47af06d63e8a7d01753c1a058ccaf6aad59
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-08-28 20:22:28 +0530

    Update 3rd_question - find_duplicate_emails.sql

--- solutions/easy/3rd_question - find_duplicate_emails.sql
+++ solutions/easy/3rd_question - find_duplicate_emails.sql
@@ -1,1 +1,18 @@
-
+-- LeetCode #182: Duplicate Emails
+-- Difficulty: Easy
+--
+-- Problem:
+-- Return duplicate mail ids.
+--
+-- Approach:
+-- Use group by for grouping similar number of emails and having to use agg. function count greater than 1 means duplicate
+--
+-- Pattern:
+-- GROUP BY 
+-- HAVING , COUNT
+--
+-- Solution:
+select email as Email 
+from Person
+group by email
+having count(*)>1 
