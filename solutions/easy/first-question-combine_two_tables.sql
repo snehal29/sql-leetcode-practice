69d8b515b5570f842adcfefcdf3c00081f52bb03
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-08-28 19:28:57 +0530

    Update first-question-combine_two_tables.sql

--- solutions/easy/first-question-combine_two_tables.sql
+++ solutions/easy/first-question-combine_two_tables.sql
@@ -1,1 +1,20 @@
+-- LeetCode #175: Combine Two Tables
+-- Difficulty: Easy
+--
+-- Problem:
+-- Return first name, last name, city and state for every person.
+-- If a person has no address, city and state should be NULL.
+--
+-- Approach:
+-- Use Person as the main table because every person must be returned.
+-- LEFT JOIN Address using personId so people without an address are retained.
+--
+-- Pattern:
+-- LEFT JOIN
+--
+-- Solution:
 
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a
+    ON p.personId = a.personId;
