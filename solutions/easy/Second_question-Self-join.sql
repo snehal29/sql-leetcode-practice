309883e7ab47d0f4ceccb6c42060d9c7813570b9
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-08-28 20:11:28 +0530

    Update Second_question-Self-join.sql

--- solutions/easy/Second_question-Self-join.sql
+++ solutions/easy/Second_question-Self-join.sql
@@ -1,1 +1,19 @@
-
+-- LeetCode #181. Employees Earning More Than Their Managers
+-- Difficulty: Easy
+--
+-- Problem:
+-- Return  name as Employee 
+--
+-- Approach:
+-- Use Self join on Employee table
+-- with condition Employee salary is greater than there manager salary.
+--
+-- Pattern:
+-- SELF JOIN
+--
+-- Solution:
+Select  e.name  as Employee 
+from Employee e
+join  Employee m
+on e.managerID =m.id
+where e.salary > m.salary 
