0f07a60b1db9fa507d986f3c0c0c7f53f877cc65
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-08-29 20:46:39 +0530

    Update 5_question-Delete-duplicate-email.sql

--- solutions/easy/5_question-Delete-duplicate-email.sql
+++ solutions/easy/5_question-Delete-duplicate-email.sql
@@ -1,1 +1,23 @@
-
+-- LeetCode #196: Delete Duplicate Emails
+-- Difficulty: Easy
+--
+-- Problem:
+-- delete all duplicate emails, keeping only one unique email with the smallest id 
+--
+-- Approach:
+-- Use ROW_NUMBER() function where partition based on email and order by id asce, which will give us unique number for each email
+-- Use Delete statement and SELF JOIN 
+-- Pattern:
+-- Delete , Self Join 
+-- Window function ROW_NUMBER() 
+--
+-- Solution:
+Delete P from Person P 
+join 
+(
+select id,
+ROW_NUMBER() over(partition by email order by id) rnk
+from Person 
+)t 
+on P.id =t.id
+where t.rnk >1;
