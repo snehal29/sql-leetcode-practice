454f942265a9d235e92cb00704a4b381d61cfdac
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-08-29 14:37:21 +0530

    Update 4_question-Customer_who_never_ordered.sql

--- solutions/easy/4_question-Customer_who_never_ordered.sql
+++ solutions/easy/4_question-Customer_who_never_ordered.sql
@@ -1,1 +1,18 @@
-
+-- LeetCode #183:Customers Who Never Order
+-- Difficulty: Easy
+--
+-- Problem:
+--  Write a solution to find all customers who never order anything
+--
+-- Approach:
+-- Use left join and IS NULL on custmerId column 
+--
+-- Pattern:
+-- LEFT JOIN and IS NULL 
+--
+--
+-- Solution:
+select c.name as Customers from Customers c
+left join Orders o 
+on c.id=o.customerId
+where o.customerId is NULL;
