feb7d3dfc85d0e19a807500722282fe98ddfb6b8
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-08-30 23:02:59 +0530

    Update 7_question-Game-play-analysis-I.sql

--- solutions/easy/7_question-Game-play-analysis-I.sql
+++ solutions/easy/7_question-Game-play-analysis-I.sql
@@ -1,1 +1,26 @@
+-- LeetCode #511:Game play analysis I
+-- Difficulty: Easy
+--
+-- Problem:
+--  Write a solution to find the first login date for each player.
+--
+-- Approach:1
+-- Use ROW_NUMBER for marking rank for each player and get only rank 1 
+-- Approach:2
+-- Use Group by on playerid and min() on event_date 
+--
+-- Pattern:
+-- ROW_NUMBER() 
+-- Group BY and MIN() 
+--
+-- Solution: 1
+select player_id , event_date as first_login
+from (select player_id , event_date ,
+ROW_NUMBER() Over(partition by player_id order by event_date) rnk
+from Activity) t
+where rnk = 1
 
+-- Solution: 2
+select player_id , min(event_date) as first_login
+from Activity
+group by player_id
