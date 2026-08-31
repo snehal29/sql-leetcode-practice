4de92b216c494030a752bc429f0d6216b62d0cb1
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-08-31 20:20:57 +0530

    Update 8_question-employee_bonus.sql

--- solutions/easy/8_question-employee_bonus.sql
+++ solutions/easy/8_question-employee_bonus.sql
@@ -1,1 +1,22 @@
-
+-- LeetCode #577: Employee Bonus
+-- Difficulty: Easy
+--
+-- Problem:
+--  Write a solution to report the name and bonus amount of each employee who satisfies either of the following:
+--          The employee has a bonus less than 1000.
+--          The employee did not get any bonus.
+--
+-- Approach:
+-- Use left join and IS NULL  
+--
+-- Pattern:
+-- LEFT JOIN and IS NULL 
+--
+--
+-- Solution:
+select e.name , b.bonus 
+from Employee e 
+left join Bonus b
+on e.empId = b.empId
+where b.bonus < 1000 or
+b.bonus is null
