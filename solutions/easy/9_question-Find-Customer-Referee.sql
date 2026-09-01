5bc02efcd7be28790303d79c463e8c91bd22ef2e
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-01 16:15:33 +0530

    Update 9_question-Find-Customer-Referee.sql

--- solutions/easy/9_question-Find-Customer-Referee.sql
+++ solutions/easy/9_question-Find-Customer-Referee.sql
@@ -1,1 +1,19 @@
-
+-- LeetCode #584:Find Customer Referee
+-- Difficulty: Easy
+--
+-- Problem:
+--  Find the names of the customer that are either:
+--  referred by any customer with id != 2.
+--  not referred by any customer.
+--
+-- Approach:
+-- Use where IS NULL , OR 
+--
+-- Pattern:
+-- where clause
+--
+--
+-- Solution:
+select name from Customer 
+where referee_id is null
+or referee_id != 2
