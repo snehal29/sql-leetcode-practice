07be1d2455337269df7fdbc0dab839aac478f3d9
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-06 18:47:45 +0530

    Create 14_question-Tree_node.sql

---
+++ solutions/easy/14_question-Tree_node.sql
@@ -0,0 +1,6 @@
+Each node in the tree can be one of three types:
+
+"Leaf": if the node is a leaf node.
+"Root": if the node is the root of the tree.
+"Inner": If the node is neither a leaf node nor a root node.
+Write a solution to report the type of each node in the tree.
