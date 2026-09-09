6ec760752b4860f5a0396e1863848c4d9cd76f9a
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-09 19:36:41 +0530

    Create 16_question-Not-boring-movie.sql

---
+++ solutions/easy/16_question-Not-boring-movie.sql
@@ -0,0 +1,19 @@
+-- LeetCode #620: Not boring Movie
+-- Difficulty: Easy
+--
+-- Problem:
+-- Report for every three line segments whether they can form a triangle.
+--
+--
+-- Approach:
+--  To find the triangle can form or not needed two sides > than 3rd one so use case when 
+-- 
+-- Pattern:
+-- CASE
+--
+-- Solution:
+select *,
+     case when (x+y > z) and (y+z> x) and ( x+z > y)  THEN 'Yes'
+     else 'No'
+     end as triangle
+from Triangle
