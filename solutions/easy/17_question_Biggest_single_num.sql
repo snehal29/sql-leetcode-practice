b1dae181e2625cdaf9aefd9b093dedaed841abc5
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-09 20:48:28 +0530

    Update 17_question_Biggest_single_num.sql

--- solutions/easy/17_question_Biggest_single_num.sql
+++ solutions/easy/17_question_Biggest_single_num.sql
@@ -2,13 +2,18 @@
 -- Difficulty: Easy
 --
 -- Problem:
---
+-- A single number is a number that appeared only once in the MyNumbers table. Find the largest single number. If there is no single number, report null.
 --
 --
 -- Approach:
---  
+--  Subquery with group by and having , count and main query max
 -- 
 -- Pattern:
--- 
+--  max , group by , having , count 
 --
 -- Solution:
+select max(num) as num from
+(select num
+from MyNumbers
+group by num
+having count(*) =1 ) t
