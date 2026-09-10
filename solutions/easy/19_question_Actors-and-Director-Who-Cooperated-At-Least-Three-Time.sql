aba845615317b15b68cf0f456aac99c817ce8bd7
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-10 21:15:07 +0530

    Update 19_question_Actors-and-Director-Who-Cooperated-At-Least-Three-Time.sql

--- solutions/easy/19_question_Actors-and-Director-Who-Cooperated-At-Least-Three-Time.sql
+++ solutions/easy/19_question_Actors-and-Director-Who-Cooperated-At-Least-Three-Time.sql
@@ -1,1 +1,18 @@
-1050. Actors and Directors Who Cooperated At Least Three Times
+-- LeetCode #1050:  Actors and Directors Who Cooperated At Least Three Times
+-- Difficulty: Easy
+--
+-- Problem:
+-- Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+--
+--
+-- Approach:
+-- group by and having count
+-- 
+-- Pattern:
+--  Group by and having and count
+--
+-- Solution:
+select actor_id , director_id 
+from ActorDirector
+group by actor_id , director_id 
+having count(*)>=3
