6761bc5afc731fee501afe6636ce30b14c9d8d3b
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-10 21:01:56 +0530

    Update 18_question_swap_sex_emp.sql

--- solutions/easy/18_question_swap_sex_emp.sql
+++ solutions/easy/18_question_swap_sex_emp.sql
@@ -1,1 +1,19 @@
-Swap Sex of Employees
+-- LeetCode #627: Swap Sex of Employees
+-- Difficulty: Easy
+--
+-- Problem:
+-- Write a solution to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement 
+-- and no intermediate temporary tables.
+--
+--
+-- Approach:
+-- Update statement with Case statement
+-- 
+-- Pattern:
+--  Update , case
+--
+-- Solution:
+Update Salary set sex = case
+  when sex='f' then 'm'
+  when sex='m' then 'f'
+end
