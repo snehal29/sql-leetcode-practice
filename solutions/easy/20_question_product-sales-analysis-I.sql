0d36a85c4e2f108dd6d94a02ed4f68aee3587423
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-13 19:39:54 +0530

    Update 20_question_product-sales-analysis-I.sql

--- solutions/easy/20_question_product-sales-analysis-I.sql
+++ solutions/easy/20_question_product-sales-analysis-I.sql
@@ -1,1 +1,17 @@
-
+-- LeetCode #1068:  Product sales analysis I
+-- Difficulty: Easy
+--
+-- Problem:
+-- Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+--
+-- Approach:
+-- Inner Join
+-- 
+-- Pattern:
+-- Inner Join
+--
+-- Solution:
+select p.product_name , s.year , s.price
+from Sales s 
+inner join Product p
+on p.product_id = s.product_id
