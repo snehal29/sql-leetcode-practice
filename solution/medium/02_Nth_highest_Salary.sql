8d22af52ce3f769fbb7f33f5bc74457cab208406
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-14 20:49:43 +0530

    Update 02_Nth_highest_Salary.sql

--- solution/medium/02_Nth_highest_Salary.sql
+++ solution/medium/02_Nth_highest_Salary.sql
@@ -5,13 +5,20 @@
 -- Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
 --
 -- Approach:
--- Window function - dense_rank 
+-- limit and offset  
 -- 
 --
 -- Pattern:
--- DENSE_RANK()
+-- offset and limit
 --
 -- Solution:
-select max(distinct salary) as SecondHighestSalary 
-from Employee 
-where salary < (select  max(distinct salary) as SecondHighestSalary  from Employee)
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    set N=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+     select distinct(salary) from Employee order by salary desc
+     limit 1 offset N
+
+  );
+END
