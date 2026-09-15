f8714f0e101407637634d704f25f8f69185b4c0f
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-15 21:26:25 +0530

    Update 21_question_project-employee_I.sql

--- solutions/easy/21_question_project-employee_I.sql
+++ solutions/easy/21_question_project-employee_I.sql
@@ -1,1 +1,18 @@
-
+-- LeetCode #1075: Project Employees I
+-- Difficulty: Easy
+--
+-- Problem:
+-- Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+--
+--
+-- Approach:
+-- Inner Join on employee_id , Group by on project_id , round() , AVG(experience_years)
+-- 
+-- Pattern:
+-- Inner Join , Group by , round() , AVG()
+--
+-- Solution:
+Update Salary set sex = case
+  when sex='f' then 'm'
+  when sex='m' then 'f'
+end
