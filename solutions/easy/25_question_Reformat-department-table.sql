4716b8b6cf9552e9d378f17564cd04e7876ee89c
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-19 23:21:30 +0530

    Update 25_question_Reformat-department-table.sql

--- solutions/easy/25_question_Reformat-department-table.sql
+++ solutions/easy/25_question_Reformat-department-table.sql
@@ -2,17 +2,30 @@
 -- Difficulty: Easy
 --
 -- Problem:
--- Write a solution to find all the authors that viewed at least one of their own articles.
---
--- Return the result table sorted by id in ascending order.
+-- Reformat the table such that there is a department id column and a revenue column for each month.
+-- The month has values in ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"].
+-- 
 --
 -- Approach:
--- compare the authore and viewer ids and sort them in sceding order 
+-- Use If condition and result also else part and sum that revenue value for each month 
 --
 -- Pattern:
--- WHERE and  ORDER BY
+-- SUM , IF , Group by and order by
 --
 -- Solution:
-select distinct author_id as id from Views 
-where author_id = viewer_id 
-order by author_id 
+select id ,
+    sum(if(month='Jan',revenue, null)) as Jan_Revenue,
+    sum(if(month='Feb',revenue, null)) as Feb_Revenue,
+    sum(if(month='Mar',revenue, null)) as Mar_Revenue,
+    sum(if(month='Apr',revenue, null)) as Apr_Revenue,
+    sum(if(month='May',revenue, null)) as May_Revenue,
+    sum(if(month='Jun',revenue, null)) as Jun_Revenue,
+    sum(if(month='Jul',revenue, null)) as Jul_Revenue,
+    sum(if(month='Aug',revenue, null)) as Aug_Revenue,
+    sum(if(month='Sep',revenue, null)) as Sep_Revenue,
+    sum(if(month='Oct',revenue, null)) as Oct_Revenue,
+    sum(if(month='Nov',revenue, null)) as Nov_Revenue,
+    sum(if(month='Dec',revenue, null)) as Dec_Revenue
+from Department
+group by id 
+order by id 
