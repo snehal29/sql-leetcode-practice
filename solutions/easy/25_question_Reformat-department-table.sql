385e1c5937647944d71ddc906cb5e61f198a2420
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-19 23:09:15 +0530

    Update 25_question_Reformat-department-table.sql

--- solutions/easy/25_question_Reformat-department-table.sql
+++ solutions/easy/25_question_Reformat-department-table.sql
@@ -1,1 +1,18 @@
-
+-- LeetCode #1179: Reformat Department Table
+-- Difficulty: Easy
+--
+-- Problem:
+-- Write a solution to find all the authors that viewed at least one of their own articles.
+--
+-- Return the result table sorted by id in ascending order.
+--
+-- Approach:
+-- compare the authore and viewer ids and sort them in sceding order 
+--
+-- Pattern:
+-- WHERE and  ORDER BY
+--
+-- Solution:
+select distinct author_id as id from Views 
+where author_id = viewer_id 
+order by author_id 
