d92725b582b7dfd208a0ab83907241434c455915
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-20 19:53:48 +0530

    Update 26_question_Quries-and-percentage.sql

--- solutions/easy/26_question_Quries-and-percentage.sql
+++ solutions/easy/26_question_Quries-and-percentage.sql
@@ -2,12 +2,18 @@
 -- Difficulty: Easy
 --
 -- Problem:
--- Write a solution to find all the authors that viewed at least one of their own articles.
---
--- Return the result table sorted by id in ascending order.
---
+-- We define query quality as: The average of the ratio between query rating and its position.
+-- We also define poor query percentage as: The percentage of all queries with rating less than 3.
+-- Write a solution to find each query_name, the quality and poor_query_percentage.
+-- Both quality and poor_query_percentage should be rounded to 2 decimal places.
+-- Explanation: 
+-- Dog queries quality is ((5 / 1) + (5 / 2) + (1 / 200)) / 3 = 2.50
+-- Dog queries poor_ query_percentage is (1 / 3) * 100 = 33.33
+
+-- Cat queries quality equals ((2 / 5) + (3 / 3) + (4 / 7)) / 3 = 0.66
+-- Cat queries poor_ query_percentage is (1 / 3) * 100 = 33.33
 -- Approach:
--- compare the authore and viewer ids and sort them in sceding order 
+-- So use group by for identifying the quality_name per group , use round() function also sum() , case when 
 --
 -- Pattern:
 -- WHERE and  ORDER BY
