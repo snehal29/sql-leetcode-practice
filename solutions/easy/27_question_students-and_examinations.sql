a6ebebd5d11fb522d87d4cf3a971806c0132a9d7
Snehal Londhe <57902757+snehal29@users.noreply.github.com>
2026-09-20 20:39:02 +0530

    Update 27_question_students-and_examinations.sql

--- solutions/easy/27_question_students-and_examinations.sql
+++ solutions/easy/27_question_students-and_examinations.sql
@@ -2,17 +2,33 @@
 -- Difficulty: Easy
 --
 -- Problem:
--- Write a solution to find all the authors that viewed at least one of their own articles.
---
--- Return the result table sorted by id in ascending order.
+-- Write a solution to find the number of times each student attended each exam.
+-- Return the result table ordered by student_id and subject_name.
+-- The result format is in the following example.
 --
 -- Approach:
--- compare the authore and viewer ids and sort them in sceding order 
+-- Cross join with subject and left join with examination , group by and order by 
 --
 -- Pattern:
--- WHERE and  ORDER BY
+-- cross and left join , group by , order by
 --
 -- Solution:
-select distinct author_id as id from Views 
-where author_id = viewer_id 
-order by author_id 
+select s.student_id ,
+     s.student_name ,
+     b.subject_name , 
+     count(e.subject_name) as attended_exams
+from 
+    Students s
+cross join 
+    Subjects b 
+Left join   
+    Examinations e  
+    on s.student_id = e.student_id
+    and  b.subject_name = e.subject_name
+group by
+     s.student_id ,
+     s.student_name,
+     b.subject_name 
+order by
+     s.student_id , 
+      b.subject_name
